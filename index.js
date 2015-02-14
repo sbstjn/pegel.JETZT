935544bf7188cf2fccfed96a244712e7e7890c72
sbstjn <mail@sbstjn.com>
2015-02-14 21:57:55 +0100

    Basic HTTP handling

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
+var lessMiddleware = require('less-middleware');
+var Pegel = require('./lib/pegel.js');
 
+/**
+ * Load Configuration
+ */
+var config = JSON.parse(fs.readFileSync('./config/app.json'));
+var mapping = JSON.parse(fs.readFileSync('./config/mapping.json'));
+
+/**
+ * Configure Express.js
+ */
+app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 5000));
+app.use(lessMiddleware(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+/**
+ * 404 - Not Found
+ */
+app.get('/404', function(req, res) {
+  res.render('404');
 });
 
+/**
+ * Index - Redirect to default
+ */
+app.get('/', function(req, res) {
+  res.redirect('/' + config.default);
+});
+
+/**
+ * Pegel
+ */
+app.get('/:location', function(req, res) {
+  if (!mapping[req.param('location')]) {
+    res.redirect('/404');
+  } else {
+    Pegel.get(mapping[req.param('location')], function(data) {
+      res.render('data', data);
+    });  
+  }
+});
+
+/**
+ * Start Express.js
+ */
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
