2e0be5296eb5531bc8f1aa6e87dd60dcca9491b2
sbstjn <sebastian.mueller@jimdo.com>
2015-03-10 10:24:42 +0100

    Rewritten location handling

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var lessMiddleware = require('less-middleware');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(lessMiddleware(__dirname + '/public', {compress: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Configure routes
-app.use('/', require('./routes.js'));
-
-module.exports = app;
+(function() {
+  var http = require('http');
+  var express = require('express');
+  var path = require('path');
+  var logger = require('morgan');
+  var lessMiddleware = require('less-middleware');
+  var app = express();
+  
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  
+  app.use(logger('dev'));
+  app.use(lessMiddleware(__dirname + '/public', {compress: false}));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  // Configure routes
+  app.use('/', require('./routes.js'));
+  
+  // Create Express.js HTTP app
+  var server = http.createServer(app);
+  exports = module.exports = server;
+  
+  // Export handler for using grunt-express
+  exports.use = function() {
+    app.use.apply(app, arguments);
+  };
+})();
--- routes.js
+++ routes.js
@@ -1,24 +1,35 @@
 (function() {
+	'use strict';
+	
 	var express = require('express');
 	var Pegelstand = require('./lib/Pegelstand.js');
 
 	var route = express();
 	var app = new Pegelstand();
 	
-	route.get('/:location', function(req, res) {
-		app.get(req.params.location, function(data) {
-			console.log(data);
-			res.send('Got location');
-		});
-	});
-	
 	route.get('/404', function(req, res) {
 		res.statusCode = 404;
 		res.send('Not Found');
 	});
 	
-	route.get('/about', function(req, res) {
-		res.render('about', {title: 'About pegel.JETZT'});
+	route.get('/impressum', function(req, res) {
+		res.render('impressum', {title: 'About pegel.JETZT'});
+	});
+	
+	route.get('/:location', function(req, res) {
+		app.get(req.params.location, function(err, data) {
+			if (err) {
+				res.redirect('/404');
+			} else {
+				data.location = true;
+				/* jshint camelcase: false */
+				data.format_time = function(h, m) {
+					return (h < 10 ? '0' : '') + h + ':' + (m < 10 ? '0' : '') + m;
+				};
+				
+				res.render('location', data);
+			}
+		});
 	});
 	
 	module.exports = exports = route;
